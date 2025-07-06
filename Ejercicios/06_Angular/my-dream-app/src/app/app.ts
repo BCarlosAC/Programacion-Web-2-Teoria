99b8d34dfd97bc7d2aef5d1d64f2c0633872ba62
main <bcarlosac20@gmail.com>
2025-07-05 19:41:45 -0500

    Generando un Componente hello-world

--- Ejercicios/06_Angular/my-dream-app/src/app/app.ts
+++ Ejercicios/06_Angular/my-dream-app/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { HelloWorldComponent } from './hello-world/hello-world';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, HelloWorldComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ Ejercicios/06_Angular/my-dream-app/src/app/hello-world/hello-world.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  templateUrl: './hello-world.html',
+  styleUrls: ['./hello-world.scss']
+})
+export class HelloWorldComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
